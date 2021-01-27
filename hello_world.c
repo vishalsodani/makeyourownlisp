f7435c4ca28f378ee839c3f24df465c60e5eab58
vishalsodani <vishalsodani@rediffmail.com>
2021-01-27 12:26:48 +0530

    working version

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,12 @@
+//use <> for the filename, then the preprocessor will look only at system include files.
 #include <stdio.h>
 
 //if no main then undefined reference to main compile error
 
+int main(int argc, char** argv){
+  puts("hello word");
+  return 0;
+}
 
 
 
