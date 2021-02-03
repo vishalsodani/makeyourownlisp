20f50dc1bd8abc73c400b4e0d25a3e086061e60d
vishalsodani <vishalsodani@rediffmail.com>
2021-02-03 14:53:19 +0530

    working parser for polish notation

--- parsing.c
+++ parsing.c
@@ -10,6 +10,22 @@
 #define PROMPT "Lispy Version 0.0.1"
 
 int main(int argc, char** argv){
+        /* Create Some Parsers */
+    mpc_parser_t* Number   = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr     = mpc_new("expr");
+    mpc_parser_t* Lispy    = mpc_new("lispy");
+
+    /* Define them with the following Language */
+    mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                     \
+        number   : /-?[0-9]+/ ;                             \
+        operator : '+' | '-' | '*' | '/' ;                  \
+        expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+        lispy    : /^/ <operator> <expr>+ /$/ ;             \
+    ",
+    Number, Operator, Expr, Lispy);
+
     puts(PROMPT);
     puts("Press ctrl+c to exit\n");
 
@@ -19,10 +35,20 @@ int main(int argc, char** argv){
 
         add_history(input);
 
-        printf("No you are a %s\n", input);
+        mpc_result_t r;
+
+        if(mpc_parse("<stdin>", input, Lispy, &r)){
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         free(input);
     }
+
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
     return 0;
 }
